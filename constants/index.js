60affe87830b9bca41f7055865547118488aac7a
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2023-09-28 16:55:30 +0530

    Commit

---
+++ constants/index.js
@@ -0,0 +1,89 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+      'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Metaverse Unleashed: A Virtual Revolution',
+
+    subtitle:
+      'Step into a new dimension where reality meets imagination.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Navigating the Metaverse: Tips and Tricks',
+    subtitle:
+      'Master the art of living in a digital universe with these expert insights.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'The Metaverse Odyssey: Explore Beyond Boundaries',
+    subtitle:
+      'Embark on a virtual journey to uncharted realms from the comfort of your home.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'github',
+    url: '/github.svg',
+  },
+];
